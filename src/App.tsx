86d987b103e4c1c65bdaaf62baae5236ffa2d67f
sanaz <sanazsyavashi1375@gmail.com>
2022-06-22 08:26:22 +0430

    main part of src

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Router from './rout';
+
+function App() {
+  return <Router />;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
+const portal: HTMLElement | Element | DocumentFragment | null | any =
+  document.getElementById('root');
+const root: any = ReactDOM.createRoot(portal);
+root.render(
+  <Provider store={store}>
+    {' '}
+    <App />{' '}
+  </Provider>
+);
